7f0ef3b8283fb143d6f3723acb6358970e25fd64
renshangui <renshangui@163.com>
2018-06-25 20:55:59 +0800

    pylint

--- ci_cd/test_string.py
+++ ci_cd/test_string.py
@@ -3,23 +3,31 @@
 #
 # Author rsg
 #
+"""test"""
+
 import unittest
 
+
 class TestStringMethods(unittest.TestCase):
+    """test case"""
 
     def test_upper(self):
+        """test upper"""
         self.assertEqual('foo'.upper(), 'FOO')
 
     def test_isupper(self):
+        """test is upper"""
         self.assertTrue('FOO'.isupper())
         self.assertFalse('Foo'.isupper())
 
     def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
+        """test split"""
+        statment = 'hello world'
+        self.assertEqual(statment.split(), ['hello', 'world'])
         # check that s.split fails when the separator is not a string
         with self.assertRaises(TypeError):
-            s.split(2)
+            statment.split(2)
+
 
 if __name__ == '__main__':
     unittest.main()
